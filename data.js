3980b6fb2d7d5c5a52bfbc2545311832d0ce0acc
rei-wa <a15dc091@dhw.ac.jp>
2019-04-01 22:51:44 +0900

    add days left

---
+++ data.js
@@ -0,0 +1,18 @@
+function ReiwaDiff() {
+  var now = new Date();
+
+        var firstDay = new Date(now.getFullYear() + 0, 4, 1);
+
+        var diff = (firstDay.getTime() - now.getTime()) / 1000;
+
+        var daysLeft = Math.floor(diff / (24 * 60 * 60));
+        var hoursLeft = (Math.floor(diff / (60 * 60))) % 24;
+        var minitesLeft = (Math.floor(diff / 60)) % 60;
+        var secondsLeft = Math.floor(diff) % 60;
+
+        minitesLeft = ("0" + minitesLeft).slice(-2)
+        secondsLeft = ("0" + secondsLeft).slice(-2)
+
+        document.getElementById("days").innerHTML = ("『令和』まであと：" + daysLeft + "日" + hoursLeft + "時間" + minitesLeft + "分" + secondsLeft + "秒です");
+}
+setInterval('ReiwaDiff()',500);
